a4c0b64fa623e7f5afdf3635b020bea491a776bf
MartinezGabi <gabi.m@hotmail.com.ar>
2023-11-09 15:44:15 -0300

    empiezo cirtuitos y tramos

--- src/ar/edu/poo2/tpFinal/CircuitoMaritimo.java
+++ src/ar/edu/poo2/tpFinal/CircuitoMaritimo.java
@@ -1,5 +1,28 @@
 package ar.edu.poo2.tpFinal;
 
-public class CircuitoMaritimo {
+import java.util.LinkedList;
+import java.util.List;
 
+public class CircuitoMaritimo {
+	
+	private List<Tramo> tramos = new LinkedList<Tramo>();
+	
+	public TerminalPortuaria getOrigen() throws Exception {
+		if (!tramos.isEmpty()) {
+			return tramos.get(0).getOrigen();
+		}
+		else throw new Exception ("no hay tramos en el circuito");
+	}
+	
+	public TerminalPortuaria getDestino() throws Exception {
+		if (!tramos.isEmpty()) {
+			return tramos.get(tramos.size() -1).getDestino();
+		}
+		else throw new Exception ("no hay tramos en el circuito");
+	}
+	
+	public void agregarTramoHaciaTerminal(TerminalPortuaria destino, double precio, int tiempo) throws Exception {
+		this.tramos.add(new Tramo(this.getDestino(), destino, precio, tiempo));
+	}
+	
 }
--- src/ar/edu/poo2/tpFinal/Tramo.java
+++ src/ar/edu/poo2/tpFinal/Tramo.java
@@ -1,5 +1,37 @@
 package ar.edu.poo2.tpFinal;
 
 public class Tramo {
+	
+	private TerminalPortuaria origen;
+	private TerminalPortuaria destino;
+	private double precio;
+	private int tiempo;
+	
+	
+	public Tramo(TerminalPortuaria origen, TerminalPortuaria destino, double precio, int tiempo) {
+		this.origen = origen;
+		this.destino = destino;
+		this.precio = precio;
+		this.tiempo = tiempo;
+	}
 
+	public TerminalPortuaria getOrigen() {
+		return origen;
+	}
+
+	public TerminalPortuaria getDestino() {
+		return destino;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getTiempo() {
+		return tiempo;
+	}
 }
